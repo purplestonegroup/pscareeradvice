28732e5a043ae1ed6020a6f31002e71d84c4261b
Silvio Vaz <hello@sevenvisualsigns.com>
2013-12-17 19:47:22 +0000

    current page in language switcher changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
 	before_action :set_i18n_locale_from_params
+	before_action :redirect_to_locale
 	# ...
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
@@ -10,12 +11,12 @@ class ApplicationController < ActionController::Base
 
 	protected
 	def set_i18n_locale_from_params
-		if params[:locale]
-			if I18n.available_locales.map(&:to_s).include?(params[:locale])
-				I18n.locale = params[:locale]
+		if params[:set_locale]
+			if I18n.available_locales.map(&:to_s).include?(params[:set_locale])
+				I18n.locale = params[:set_locale]
 			else
 				flash.now[:notice] =
-					"#{params[:locale]} translation not available"
+					"#{params[:set_locale]} translation not available"
 				logger.error flash.now[:notice]
 			end
 		end
@@ -23,6 +24,20 @@ class ApplicationController < ActionController::Base
 
 	def default_url_options
 		{ locale: I18n.locale }		
+	end
+
+	helper_method :current_path
+	def current_path
+		url = request.env["PATH_INFO"]
+		Rails.application.routes.recognize_path url
+	end
+
+	def redirect_to_locale
+		if params[:set_locale].present?
+			route = current_path
+			route[:locale] = I18n.locale
+  			redirect_to route
+  		end
 	end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,7 @@ class PostsController < ApplicationController
   end
 
   def home
-  	if params[:set_locale]
-  		redirect_to root_url(locale: params[:set_locale])
-  	else
-  		@posts = Post.all.order("created_at desc")
-  	end
+  	@posts = Post.all.order("created_at desc")
   end
 
 end
