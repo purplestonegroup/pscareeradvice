1c34007f4d783f6065459c68d83ecb77888d94d4
Silvio Vaz <hello@sevenvisualsigns.com>
2013-12-22 22:06:25 +0000

    changing to the initial state of the i18n switcher to index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
 
 	before_action :set_i18n_locale_from_params
-	before_action :redirect_to_locale
 	# ...
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
@@ -11,12 +10,12 @@ class ApplicationController < ActionController::Base
 
 	protected
 	def set_i18n_locale_from_params
-		if params[:set_locale]
-			if I18n.available_locales.map(&:to_s).include?(params[:set_locale])
-				I18n.locale = params[:set_locale]
+		if params[:locale]
+			if I18n.available_locales.map(&:to_s).include?(params[:locale])
+				I18n.locale = params[:locale]
 			else
 				flash.now[:notice] =
-					"#{params[:set_locale]} translation not available"
+					"#{params[:locale]} translation not available"
 				logger.error flash.now[:notice]
 			end
 		end
@@ -24,20 +23,6 @@ class ApplicationController < ActionController::Base
 
 	def default_url_options
 		{ locale: I18n.locale }		
-	end
-
-	helper_method :current_path
-	def current_path
-		url = request.env["PATH_INFO"]
-		Rails.application.routes.recognize_path url
-	end
-
-	def redirect_to_locale
-		if params[:set_locale].present?
-			route = current_path
-			route[:locale] = I18n.locale
-  			redirect_to route
-  		end
 	end
 
 end
