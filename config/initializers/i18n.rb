a47ec5c5b119e70c85ca39f03559ff191fcd7554
Silvio Vaz <hello@sevenvisualsigns.com>
2014-02-08 19:25:35 +0000

    adding the rumanian language

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -4,5 +4,6 @@ I18n.default_locale = :en
 LANGUAGES = [
 	['English', 'en'],
 	["Espa&ntilde;ol".html_safe, 'es'],
-	["Fran&ccedil;ais".html_safe, 'fr']
+	["Fran&ccedil;ais".html_safe, 'fr'],
+	["Rom&acirc;n&#x103;".html_safe, 'rm']
 ]
